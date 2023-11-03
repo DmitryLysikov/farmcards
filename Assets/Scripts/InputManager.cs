3f5a629a847a1f13c1dad29d568803afa15f9729
EmuTheGreat <thegreatfrut@mail.ru>
2023-11-03 16:59:30 +0500

    first commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField]
+    private LayerMask placementLayermask;
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldPosition = sceneCamera.ScreenToWorldPoint(mousePos);
+        worldPosition.z = 0f;
+        return worldPosition;
+    }
+}
