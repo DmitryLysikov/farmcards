2a8ec1ecdf648ad34ae1ac18a02e6e8fbebbdc32
EmuTheGreat <thegreatfrut@mail.ru>
2023-11-03 17:53:50 +0500

    functionality added

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,10 +9,19 @@ public class InputManager : MonoBehaviour
     [SerializeField]
     private Camera sceneCamera;
 
-    private Vector3 lastPosition;
-
     [SerializeField]
     private LayerMask placementLayermask;
+    
+    public event Action OnClicked, OnExit;
+    public bool IsPointOverUI() => EventSystem.current.IsPointerOverGameObject();
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) 
+            OnClicked?.Invoke();
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OnExit?.Invoke();
+    }
 
     public Vector3 GetSelectedMapPosition()
     {
--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -8,13 +8,64 @@ public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
     private InputManager inputManager;
+
     [SerializeField]
     private GameObject mouseIndicator, cellIndicator;
+
     [SerializeField]
     private Grid grid;
 
+    [SerializeField]
+    private ObjectsDatabaseSO dataBase;
+    private int selectedObjectIndex = -1;
+
+    [SerializeField]
+    private GameObject gridVisualisation;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        selectedObjectIndex = dataBase.objectsData.FindIndex(data => data.ID == ID);
+        if(selectedObjectIndex < 0)
+        {
+            Debug.LogError($"No ID found {ID}");
+        }
+        gridVisualisation.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        if (inputManager.IsPointOverUI())
+        {
+            return;
+        }
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newObject = Instantiate(dataBase.objectsData[selectedObjectIndex].Prefab);
+        newObject.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualisation.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
     private void Update()
     {
+        if(selectedObjectIndex < 0) 
+            return;
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
